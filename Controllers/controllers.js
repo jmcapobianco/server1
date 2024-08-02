7c3fcdcf6f395a2add2feaf4766f4e291dbc5805
Juan Manuel Capobianco <j.m.capobianco72@gmail.com>
2024-08-01 22:37:45 -0300

    cambiesitos

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -1,8 +1,45 @@
+import { readData, writeData } from "../utils/utils";
+
+
 // getters -> funciones que toman datos de la db
-const getPizzas = () => {};
-const getPizzasById = () => {};
+const getPizzas = () => {
+    const pizzas = readData();
+    return pizzas;
+};
 
-//setters -> funciones que modifican la db
 
-const addPizza = () => {};
-const deletePizzaById = () => {};
+const getPizzasById = (id) => {
+    const pizzas = readData();
+    const pizza = pizzas.find((pizza) => pizza.id === id);
+    return pizza;
+};
+
+
+const addPizza = (nuevaPizza) => {
+    const pizzas = readData();
+    const pizza = pizzas.find((pizza) => pizza.nombre === id);
+    if (pizza) {
+         return "Ya existe la pizzarda"
+    } else {
+        pizzas.push(nuevaPizza);
+        writeData(JSON.stringify(pizzas));
+        return "Pizza agregada con exito!"
+    }
+};
+
+addPizza({id: 11, ingredientes: ["Tomate", "Queso"], nombre: "la boss", precio: 140, tamaño: "grande",});
+
+const deletePizzaById = (id) => {
+    const pizzas = readData();
+    const pizza = pizzas.find (pizza => pizza.id !== id);
+    if (pizza) {
+        const nuevasPizzas = pizzas.filter((pizza) => pizza.id !==id);
+        writeData(JSON.stringify(nuevasPizzas));
+        return "Pizza borrada con exito.";
+    }
+    return "La pizza no existe";
+};
+
+console.log(deletePizzaById(1));
+
+export {getPizzas, getPizzasById, addPizza, deletePizzaById};
--- utils/utils.js
+++ utils/utils.js
@@ -1,11 +1,15 @@
 import fs from "node:fs";
 
 const readData = () => {
-    const jsonData = fs.readFileSync("./database/pizza.json");
+    const jsonData = fs.readFileSync("./database/pizza.json", "utf8");
     const pizzas = JSON.parse(jsonData);
     return pizzas;
 };
 
-const writeData = () => {};
+const writeData = (data) => {
+    fs.writeFileSync(dataFilePath, data, 'utf8');
+};
 
 console.log(readData());
+
+export { readData, writeData };
